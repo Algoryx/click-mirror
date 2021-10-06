d32732faaaa599e053f3aae67316847d94462e5a
Martin Törnqvist <martin.tornqvist@algoryx.se>
2021-10-06 09:43:12 +0200

    Fix DLL export preprocessor define

--- cpp-src/click/include/click/DllExport.h
+++ cpp-src/click/include/click/DllExport.h
@@ -1,6 +1,6 @@
 #pragma once
 #if defined(_MSC_VER) && defined(BUILD_SHARED_LIBS)
-  #if defined(CLICK_EXPORTS)
+  #if defined(clicklib_EXPORTS)
 #define CLICK_EXPORT __declspec(dllexport)
   #else
 #define CLICK_EXPORT __declspec(dllimport)
