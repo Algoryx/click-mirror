0a26178435b80c7cbfb01dbf6a7fbf97aec10859
Jonas Andersson <jonas.andersson@algoryx.se>
2021-11-18 09:57:57 +0100

    Release version 0.1.13

--- python-src/setup.py
+++ python-src/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pClick",
-    version="0.1.12",
+    version="0.1.13",
     author="Algoryx Simulation",
     author_email="contact@algoryx.se",
     description="Controller to AGX messaging",
