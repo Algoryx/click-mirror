2e88f26c4c58cf96255b4bd9030ee3ccaad29e4a
Jonas Andersson <jonas.andersson@algoryx.se>
2021-11-23 15:29:17 +0100

    Add Docstrings for messages and builders

--- cpp-src/click/include/click/ErrorMessageBuilder.h
+++ cpp-src/click/include/click/ErrorMessageBuilder.h
@@ -6,8 +6,23 @@
 class click::ErrorMessageBuilder {
 
 public:
+    /**
+     * Build the message
+     *
+     * \return a ErrorMessage
+     */
     CLICK_EXPORT std::unique_ptr<ErrorMessage> build();
+    /**
+     * Create a builder that creates a Message
+     *
+     * \return a Builder
+     */
     CLICK_EXPORT static std::unique_ptr<ErrorMessageBuilder> builder();
+    /**
+     * Add informative text to the error message
+     *
+     * \return a Builder
+     */
     CLICK_EXPORT ErrorMessageBuilder * withMessage(const std::string &);
     CLICK_EXPORT virtual ~ErrorMessageBuilder();
 
--- cpp-src/click/include/click/ResetMessageBuilder.h
+++ cpp-src/click/include/click/ResetMessageBuilder.h
@@ -6,7 +6,17 @@
 class click::ResetMessageBuilder {
 
 public:
+    /**
+     * Build the message
+     *
+     * \return a ResetMessage
+     */
     CLICK_EXPORT std::unique_ptr<ResetMessage> build();
+    /**
+     * Create a builder that creates a Message
+     *
+     * \return a Builder
+     */
     CLICK_EXPORT static std::unique_ptr<ResetMessageBuilder> builder();
     CLICK_EXPORT virtual ~ResetMessageBuilder();
 
