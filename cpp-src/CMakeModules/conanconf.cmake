1d425e29f9973565d2fc0a09aa63dfc156dcaf4e
Jonas Andersson <jonas.andersson@algoryx.se>
2021-11-26 11:19:13 +0100

    Add TLS_VERIFY

--- cpp-src/CMakeModules/conanconf.cmake
+++ cpp-src/CMakeModules/conanconf.cmake
@@ -5,9 +5,10 @@ set(CONAN_CMAKE_LOCAL_FILE ${CMAKE_BINARY_DIR}/conan.cmake)
 set(CONAN_CMAKE_MASTER_FILE ${CMAKE_BINARY_DIR}/.github.conan.cmake)
 
 if(NOT EXISTS ${CONAN_CMAKE_MASTER_FILE})
-  message(STATUS "Downloading conan.cmake from https://github.com/memsharded/cmake-conan")
+  message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
   file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
-                "${CONAN_CMAKE_MASTER_FILE}" STATUS CONAN_CMAKE_DOWNLOAD_STATUS)
+                "${CONAN_CMAKE_MASTER_FILE}" STATUS CONAN_CMAKE_DOWNLOAD_STATUS
+                TLS_VERIFY ON)
 
   list(GET CONAN_CMAKE_DOWNLOAD_STATUS 0 CONAN_CMAKE_DOWNLOAD_STATUS)
 
@@ -45,7 +46,6 @@ conan_cmake_autodetect(settings)
 
 conan_cmake_install(PATH_OR_REFERENCE .
                       BUILD missing
-#                      REMOTE conancenter
                       SETTINGS ${settings})
 
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake) 
