59b03207196e718426dcfa7e6ad57d96a99ea953
Jonas Andersson <jonas.andersson@algoryx.com>
2025-09-03 14:19:14 +0200

    Skipping Client-Server integration test on Windows, ipc is not supported there

--- cpp-src/click/tests/IntegrationTestClientServer.cpp
+++ cpp-src/click/tests/IntegrationTestClientServer.cpp
@@ -1,6 +1,4 @@
-#if defined(_WIN32)
-#include <io.h> // For _mktemp on Windows
-#else
+#if !defined(_WIN32)
 #include <unistd.h> // For mkstemp on Unix-like systems
 #endif
 #include <catch2/catch_all.hpp>
@@ -15,29 +13,22 @@ using namespace std;
 using namespace click;
 
 
+#if !defined(_WIN32) // Skipping Client-Server integration test on Windows, ipc is not supported there.
 SCENARIO("Client-Server integration test", "[click]")
 {
-#if defined(_WIN32)
-    CATCH_SKIP("Skipping Client-Server integration test on Windows, ipc is not supported there.");
-#endif
     GIVEN("A client and a server")
     {
+        Client client;
+        Server server;
+        unique_ptr<Message> server_message;
+        char tmp_filename[] = "/tmp/click_test_XXXXXX";
+        int fd = mkstemp(tmp_filename); // Secure temporary file creation
+        close(fd); // Close the file descriptor immediately
+        std::string endpoint = std::string("ipc://") + tmp_filename;
         WHEN("Sending a HandshakeInitMessage")
         {
-            Client client;
-            Server server;
-            unique_ptr<Message> server_message;
-            char tmp_filename[] = "/tmp/click_test_XXXXXX";
-#if defined(_WIN32)
-            _mktemp(tmp_filename); // Windows-specific temporary file creation
-#else
-            int fd = mkstemp(tmp_filename); // Secure temporary file creation
-            close(fd); // Close the file descriptor immediately
-#endif
-            std::string endpoint = std::string("ipc://") + tmp_filename;
             client.connect(endpoint);
             server.bind(endpoint);
-
             unique_ptr<Message> client_message = HandshakeInitMessageBuilder::builder()->build();
             client.send(*client_message);
             unique_ptr<Message> server_reply = server.receive(true);
@@ -51,3 +42,4 @@ SCENARIO("Client-Server integration test", "[click]")
         }
     }
 }
+#endif
